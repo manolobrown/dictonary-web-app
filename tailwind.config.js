f19d4001827fd06c8ae98836cc95e3bd6ceac8f5
Manuel Pena <manolobrown@users.noreply.github.com>
2023-07-06 18:34:56 -0400

    [UPDATE] Google Fonts Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Lora", "serif"],
+        mono: ["Inconsolata", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
